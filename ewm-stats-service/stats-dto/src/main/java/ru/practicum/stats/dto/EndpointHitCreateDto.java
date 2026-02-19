7cf1461ad0df8d640d4c5a01a8a20263b8b361ce
Dmitry Karfidov <dmitry.karfidov@gmail.com>
2026-02-19 20:22:34 +0300

    Поменял в DTO классы record на обычные Java-классы (тестирование PR на GitHub не поддерживает record)

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitCreateDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitCreateDto.java
@@ -7,22 +7,47 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record EndpointHitCreateDto(
+public class EndpointHitCreateDto {
 
         @NotBlank
         @Size(max = 64)
-        String app,
+        private final String app;
 
         @NotBlank
         @Size(max = 128)
-        String uri,
+        private final String uri;
 
         @NotBlank
         @Size(max = 45)
-        String ip,
+        private final String ip;
 
         @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime timestamp
-) {
+        private final LocalDateTime timestamp;
+
+        public EndpointHitCreateDto(String app,
+                                    String uri,
+                                    String ip,
+                                    LocalDateTime timestamp) {
+                this.app = app;
+                this.uri = uri;
+                this.ip = ip;
+                this.timestamp = timestamp;
+        }
+
+        public String getApp() {
+                return app;
+        }
+
+        public String getUri() {
+                return uri;
+        }
+
+        public String getIp() {
+                return ip;
+        }
+
+        public LocalDateTime getTimestamp() {
+                return timestamp;
+        }
 }
--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
@@ -1,8 +1,26 @@
 package ru.practicum.stats.dto;
 
-public record ViewStatsDto(
-        String app,
-        String uri,
-        long hits
-) {
+public class ViewStatsDto {
+
+    private final String app;
+    private final String uri;
+    private final long hits;
+
+    public ViewStatsDto(String app, String uri, long hits) {
+        this.app = app;
+        this.uri = uri;
+        this.hits = hits;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public long getHits() {
+        return hits;
+    }
 }
